f817da4ede8e4901507a3c32e4513fa018923805
vaca <wsmslgh@gmail.com>
2022-05-13 18:56:25 -0700

    asd

--- main/main.c
+++ main/main.c
@@ -1,19 +1,8 @@
-/* HTTP File Server Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <sys/unistd.h>
 #include <sys/stat.h>
 #include <sys/param.h>
-
-
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -23,39 +12,25 @@
 #include "esp_err.h"
 #include "esp_vfs_fat.h"
 #include "nvs_flash.h"
-
 #include "sdkconfig.h"
 #include "driver/sdspi_host.h"
 #include "driver/spi_common.h"
 #include "sdmmc_cmd.h"
 #include "soc/soc_caps.h"
-
-
 #include "driver/sdmmc_host.h"
-
 #include <freertos/event_groups.h>
 #include <soc/i2s_reg.h>
 #include <soc/rtc.h>
 #include <driver/periph_ctrl.h>
 
-/* This example demonstrates how to create file server
- * using esp_http_server. This file has only startup code.
- * Look in file_server.c for the implementation */
-
 #define MOUNT_POINT "/sdcard"
 static const char *TAG = "example";
-/* ESP32-S2/C3 doesn't have an SD Host peripheral, always use SPI,
- * ESP32 can choose SPI or SDMMC Host, SPI is used by default: */
 
 #define EXAMPLE_ESP_WIFI_SSID "vaca"
 #define EXAMPLE_ESP_WIFI_PASS "22345678"
 #define EXAMPLE_ESP_MAXIMUM_RETRY 32
 
 static EventGroupHandle_t s_wifi_event_group;
-/* The event group allows multiple bits for each event, but we only care about
- * two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 
@@ -68,7 +43,6 @@ static char *mount_base_path = MOUNT_POINT;
 esp_err_t start_file_server(const char *base_path);
 
 void sdcard_mount2(void) {
-    /*sd_card part code*/
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
             .format_if_mount_failed = false,
             .max_files = 5,
@@ -77,28 +51,12 @@ void sdcard_mount2(void) {
     sdmmc_card_t *card;
     const char mount_point[] = MOUNT_POINT;
     ESP_LOGI(TAG, "Initializing SD card");
-
-
     ESP_LOGI(TAG, "Using SDMMC peripheral");
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
     host.max_freq_khz = SDMMC_FREQ_HIGHSPEED;
 
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
-    // To use 1-line SD mode, uncomment the following line:
-     slot_config.width = 1;
-
-    // GPIOs 15, 2, 4, 12, 13 should have external 10k pull-ups.
-    // Internal pull-ups are not sufficient. However, enabling internal pull-ups
-    // does make a difference some boards, so we do that here.
-//    gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
-//    gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);    // D0, needed in 4- and 1-line modes
-//    gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);    // D1, needed in 4-line mode only
-//    gpio_set_pull_mode(12, GPIO_PULLUP_ONLY);   // D2, needed in 4-line mode only
-//    gpio_set_pull_mode(13, GPIO_PULLUP_ONLY);   // D3, needed in 4- and 1-line modes
-
+    slot_config.width = 1;
     esp_err_t ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
 
     if (ret != ESP_OK) {
@@ -130,7 +88,7 @@ static void event_handler(void *arg, esp_event_base_t event_base,
         }
         ESP_LOGI(TAG, "connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
@@ -160,13 +118,7 @@ void wifi_init_sta(void) {
                     {
                             .ssid = EXAMPLE_ESP_WIFI_SSID,
                             .password = EXAMPLE_ESP_WIFI_PASS,
-                            /* Setting a password implies station will connect to all
-                             * security modes including WEP/WPA. However these modes are
-                             * deprecated and not advisable to be used. Incase your Access
-                             * point doesn't support WPA2, these mode can be enabled by
-                             * commenting below line */
                             .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-
                             .pmf_cfg = {.capable = true, .required = false},
                     },
     };
@@ -176,16 +128,11 @@ void wifi_init_sta(void) {
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT)
-     * or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler()
-     * (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
                                            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
                                            pdFALSE, pdFALSE, portMAX_DELAY);
 
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we
-     * can test which event actually happened. */
+
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
                  EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
@@ -198,7 +145,6 @@ void wifi_init_sta(void) {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
-    /* The event will not be processed after unregister */
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
             IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
