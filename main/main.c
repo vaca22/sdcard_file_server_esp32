a1cfb8ed287b602b91072bc50a7cb9136636e831
vaca <wsmslgh@gmail.com>
2022-06-01 07:43:28 -0700

    asfdgdfgfhggfhdfdsffdsgafdrgtsdfsdffdghertg54dr

--- main/main.c
+++ main/main.c
@@ -173,6 +173,7 @@ int UDP_PORT = 8889;
 
 
 char *vaca = "vaca";
+char *vacax = "xvacax";
 
 
 static void udp_server_task(void *pvParameters) {
@@ -243,7 +244,7 @@ static void udp_server_task(void *pvParameters) {
                     }
                 }
                 if(valid){
-                    int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *) &source_addr, sizeof(source_addr));
+                    int err = sendto(sock, vacax, 7, 0, (struct sockaddr *) &source_addr, sizeof(source_addr));
                     if (err < 0) {
                         ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
                         break;
