e140920273044f93bddbd220878cf6f3f8d86a74
vaca <wsmslgh@gmail.com>
2022-06-01 10:18:36 -0700

    asfdgdfgfhggfhdfdsffdsgafdrgtsdfdgfsdffdghertg54drfdg

--- main/main.c
+++ main/main.c
@@ -169,7 +169,7 @@ void initEthernet() {
 
 
 int UDP_PORT = 8889;
-
+int TCP_PORT = 13207;
 
 
 char *vaca = "vaca";
@@ -236,14 +236,14 @@ static void udp_server_task(void *pvParameters) {
                 rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
                 ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
                 ESP_LOGI(TAG, "%s", rx_buffer);
-                int valid=1;
-                for(int k=0;k<4;k++){
-                    if(rx_buffer[k]!=vaca[k]){
-                        valid=0;
+                int valid = 1;
+                for (int k = 0; k < 4; k++) {
+                    if (rx_buffer[k] != vaca[k]) {
+                        valid = 0;
                         break;
                     }
                 }
-                if(valid){
+                if (valid) {
                     int err = sendto(sock, vacax, 6, 0, (struct sockaddr *) &source_addr, sizeof(source_addr));
                     if (err < 0) {
                         ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
@@ -260,6 +260,105 @@ static void udp_server_task(void *pvParameters) {
             close(sock);
         }
     }
+    vTaskDelete(NULL);
+}
+
+
+static void do_retransmit(const int sock) {
+    int len;
+    char rx_buffer[128];
+    len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+    if (len < 0) {
+        ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+    } else if (len == 0) {
+        ESP_LOGW(TAG, "Connection closed");
+    } else {
+        rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+        ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
+        int written = send(sock, vacax, 6, 0);
+        if (written < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+        }
+    }
+}
+
+static void tcp_server_task(void *pvParameters) {
+    char addr_str[128];
+    int addr_family = (int) pvParameters;
+    int ip_protocol = 0;
+    int keepAlive = 1;
+    int keepIdle = 5;
+    int keepInterval = 5;
+    int keepCount = 3;
+    struct sockaddr_storage dest_addr;
+
+    if (addr_family == AF_INET) {
+        struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *) &dest_addr;
+        dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+        dest_addr_ip4->sin_family = AF_INET;
+        dest_addr_ip4->sin_port = htons(TCP_PORT);
+        ip_protocol = IPPROTO_IP;
+    }
+
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+
+    ESP_LOGI(TAG, "Socket created");
+
+    int err = bind(listen_sock, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
+        goto CLEAN_UP;
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", TCP_PORT);
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        goto CLEAN_UP;
+    }
+
+    while (1) {
+
+        ESP_LOGI(TAG, "Socket listening");
+
+        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t addr_len = sizeof(source_addr);
+        int sock = accept(listen_sock, (struct sockaddr *) &source_addr, &addr_len);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+
+        // Set tcp keepalive option
+        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
+        // Convert ip address to string
+        if (source_addr.ss_family == PF_INET) {
+            inet_ntoa_r(((struct sockaddr_in *) &source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+        }
+
+        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+
+        do_retransmit(sock);
+
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+    CLEAN_UP:
+    close(listen_sock);
     vTaskDelete(NULL);
 }
 
@@ -284,6 +383,7 @@ void app_main(void) {
 
 
     xTaskCreate(udp_server_task, "udp_server", 4096, (void *) AF_INET, 5, NULL);
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
     sdcard_mount2();
     start_file_server();
     vEventGroupDelete(s_wifi_event_group);
