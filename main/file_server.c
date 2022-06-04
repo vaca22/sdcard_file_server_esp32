58aa9a1546a16342865621b842cbea2b9c4de614
vaca <wsmslgh@gmail.com>
2022-06-04 02:18:48 -0700

    asfdgdfgfhggfhdfdsffdsgafdrgtsdfdgfsdffdghertg54drfdgRETfgdfvfgsdfffddsdffgrtgdfgdf

--- main/file_server.c
+++ main/file_server.c
@@ -60,6 +60,8 @@
 #include <soc/rtc.h>
 #include <driver/periph_ctrl.h>
 #include <cJSON.h>
+#include <lwip/sockets.h>
+
 extern int haveSD;
 static TaskHandle_t chem1_task_h;
 FILE *playFile = NULL;
@@ -751,8 +753,104 @@ static void chem1_task(void *pvParameters) {
         vTaskDelay(1000);
     }
 }
+int UDP_PORT = 8889;
+
+
+
+char *vaca = "vaca";
+char *vacax = "xvacax";
+
+
+static void udp_server_task(void *pvParameters) {
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family = (int) pvParameters;
+    int ip_protocol = 0;
+    struct sockaddr_in6 dest_addr;
+
+    while (1) {
+
+        if (addr_family == AF_INET) {
+            struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *) &dest_addr;
+            dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+            dest_addr_ip4->sin_family = AF_INET;
+            dest_addr_ip4->sin_port = htons(UDP_PORT);
+            ip_protocol = IPPROTO_IP;
+        } else if (addr_family == AF_INET6) {
+            bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
+            dest_addr.sin6_family = AF_INET6;
+            dest_addr.sin6_port = htons(UDP_PORT);
+            ip_protocol = IPPROTO_IPV6;
+        }
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+
+        int err = bind(sock, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        }
+        ESP_LOGI(TAG, "Socket bound, port %d", UDP_PORT);
+
+        while (1) {
+
+            ESP_LOGI(TAG, "Waiting for data");
+            struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *) &source_addr, &socklen);
+
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+                // Data received
+            else {
+                // Get the sender's ip address as string
+                if (source_addr.ss_family == PF_INET) {
+                    inet_ntoa_r(((struct sockaddr_in *) &source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+                } else if (source_addr.ss_family == PF_INET6) {
+                    inet6_ntoa_r(((struct sockaddr_in6 *) &source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
+                }
+
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+                int valid = 1;
+                for (int k = 0; k < 4; k++) {
+                    if (rx_buffer[k] != vaca[k]) {
+                        valid = 0;
+                        break;
+                    }
+                }
+                if (valid) {
+
+                    int err = sendto(sock, vacax, 6, 0, (struct sockaddr *) &source_addr, sizeof(source_addr));
+                    if (err < 0) {
+                        ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
 
 esp_err_t start_file_server() {
+    xTaskCreate(udp_server_task, "udp_server", 4096, (void *) AF_INET, 5, NULL);
     if(haveSD==0){
         play_ring_buffer= malloc(total);
     }
--- main/main.c
+++ main/main.c
@@ -169,119 +169,15 @@ void initEthernet() {
 }
 
 
-int UDP_PORT = 8889;
+
 int TCP_PORT = 13207;
 
 
-char *vaca = "vaca";
-char *vacax = "xvacax";
 
 
-static void udp_server_task(void *pvParameters) {
-    char rx_buffer[128];
-    char addr_str[128];
-    int addr_family = (int) pvParameters;
-    int ip_protocol = 0;
-    struct sockaddr_in6 dest_addr;
-
-    while (1) {
-
-        if (addr_family == AF_INET) {
-            struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *) &dest_addr;
-            dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
-            dest_addr_ip4->sin_family = AF_INET;
-            dest_addr_ip4->sin_port = htons(UDP_PORT);
-            ip_protocol = IPPROTO_IP;
-        } else if (addr_family == AF_INET6) {
-            bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
-            dest_addr.sin6_family = AF_INET6;
-            dest_addr.sin6_port = htons(UDP_PORT);
-            ip_protocol = IPPROTO_IPV6;
-        }
-
-        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(TAG, "Socket created");
 
 
-        int err = bind(sock, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
-        if (err < 0) {
-            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-        }
-        ESP_LOGI(TAG, "Socket bound, port %d", UDP_PORT);
 
-        while (1) {
-
-            ESP_LOGI(TAG, "Waiting for data");
-            struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
-            socklen_t socklen = sizeof(source_addr);
-            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *) &source_addr, &socklen);
-
-            // Error occurred during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
-                break;
-            }
-                // Data received
-            else {
-                // Get the sender's ip address as string
-                if (source_addr.ss_family == PF_INET) {
-                    inet_ntoa_r(((struct sockaddr_in *) &source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
-                } else if (source_addr.ss_family == PF_INET6) {
-                    inet6_ntoa_r(((struct sockaddr_in6 *) &source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
-                }
-
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(TAG, "%s", rx_buffer);
-                int valid = 1;
-                for (int k = 0; k < 4; k++) {
-                    if (rx_buffer[k] != vaca[k]) {
-                        valid = 0;
-                        break;
-                    }
-                }
-                if (valid) {
-                    int err = sendto(sock, vacax, 6, 0, (struct sockaddr *) &source_addr, sizeof(source_addr));
-                    if (err < 0) {
-                        ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                        break;
-                    }
-                }
-
-            }
-        }
-
-        if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
-    }
-    vTaskDelete(NULL);
-}
-
-
-static void do_retransmit(const int sock) {
-    int len;
-    char rx_buffer[128];
-    len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-    if (len < 0) {
-        ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-    } else if (len == 0) {
-        ESP_LOGW(TAG, "Connection closed");
-    } else {
-        rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-        ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
-        int written = send(sock, vacax, 6, 0);
-        if (written < 0) {
-            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-        }
-    }
-}
 
 static void tcp_server_task(void *pvParameters) {
     char addr_str[128];
@@ -352,7 +248,6 @@ static void tcp_server_task(void *pvParameters) {
 
         ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
 
-        do_retransmit(sock);
 
         shutdown(sock, 0);
         close(sock);
@@ -454,7 +349,7 @@ void app_main(void) {
                         portMAX_DELAY);
 
 
-    xTaskCreate(udp_server_task, "udp_server", 4096, (void *) AF_INET, 5, NULL);
+
     xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
     if(gpio_get_level(GPIO_INPUT_IO_0)==0){
         haveSD=1;
